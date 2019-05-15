57e4807432a07d07070df5ee2e2928dc35dccabf
yinghuang <huangyinga@hotmail.com>
2019-05-15 16:57:07 +0800

    add tcp-hello

--- httpHello.go
+++ httpHello.go
@@ -6,10 +6,15 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Hello %s!</h1>", r.RemoteAddr)
+}
+
+func user_handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>Hello %s!</h1>", r.URL.Path)
 }
 
 func main() {
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/user", user_handler)
 	http.ListenAndServe(":8080", nil)
 }

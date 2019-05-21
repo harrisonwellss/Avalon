1cc19bcb8a844476864963594a1822371db30687
yinghuang <huangyinga@hotmail.com>
2019-05-21 16:10:52 +0800

    add 4

--- 4/array.go
+++ 4/array.go
@@ -1,9 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"unsafe"
+)
 
 func main() {
 	q2 := [...]int{4: 2, 10: -1}
 
 	fmt.Println(len(q2))
+
+	a1 := [3]int{1, 2, 3}
+
+	var a2 [3]int
+
+	a2 = a1
+
+	fmt.Println(&a1[0], &a2[0])
+	fmt.Println(unsafe.Sizeof(a1))
+
+	a2[1] = 99
+
+	fmt.Println(a1, a2)
 }
---
+++ 4/md5.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"crypto/md5"
+	"fmt"
+)
+
+func main() {
+	data := []byte("hello")
+	md5sum := md5.Sum(data)
+	fmt.Printf("%x\n", md5sum)
+	//fmt.Printf(md5sum)
+}
